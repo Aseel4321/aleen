82d8fbea44cc7abaeda59aee8db78152b5a2a495
Aseel4321 <aseelghaleb2088@gmail.com>
2025-04-09 17:17:40 +0300

    ssssasa

--- lib/Model/Login_Api.dart
+++ lib/Model/Login_Api.dart
@@ -1,15 +1,6 @@
 import 'dart:convert' as convert;
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+
 class Login_Api{
 
-static Future Login()async{
-var t=await http.post(Uri.parse('http://192.168.88.9:9090/api/users/signin'),body:{
- "emailOrUsernameOrPhoneNumber":"test@gmail.com",
- "password": "test1234"
-}
-);
-print(t.body);
-return t.body;
-}
 }
